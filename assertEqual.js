a6984e61ff7036b46c4336967c0bea2746a29b75
Francisco <kikopocampo@gmail.com>
2022-10-17 15:50:05 -0600

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🥲Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual('Bootcamp', 'Bootcamp');
+assertEqual(2,1);
