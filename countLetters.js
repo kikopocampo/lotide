31f4343907d21d3d3991dbeb9db6af44dde3fa9f
Francisco <kikopocampo@gmail.com>
2022-10-19 14:00:21 -0600

    collects info in an obj based on the number of occurence in a string(minus the spaces)

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🥲Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const compressSentence = sentence => sentence.replace(/ /g,'');
+
+const countLetters = function(sentence) {
+  let output = {};
+  for (const letters of sentence) {
+    output[letters] ? output[letters] ++ : output[letters] = 1;
+  }
+  return output;
+};
+
+const test1 = (countLetters(compressSentence('lighthouse in the house')));
+const test2 = (countLetters(compressSentence('aabb')));
+
+assertEqual(test1['l'], 1);
+assertEqual(test1['e'], 3);
+assertEqual(test2['a'], 2);
+assertEqual(test2['b'], 2);
