f9f7b2f6d1729fccf4e0bb6dd0b6916199a143e3
Francisco <kikopocampo@gmail.com>
2022-10-18 19:47:04 -0600

    fixed indentations

--- flatten.js
+++ flatten.js
@@ -33,3 +33,4 @@ const flatten = function(arr){
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
 console.log(flatten([[1, 2], [3, 4], 5, [6, 7]]));
 console.log(flatten([1, [2], [3, 4, 5], [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1,2,3,4,5,6])
