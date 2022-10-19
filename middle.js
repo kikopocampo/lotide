955714edc8bc34c3677dc5f6070cb63bda27e084
Francisco <kikopocampo@gmail.com>
2022-10-18 21:23:26 -0600

    changed indentations

--- middle.js
+++ middle.js
@@ -1,6 +1,6 @@
 const lenCheck = function(arr1,arr2) {
-  if (!Array.isArray(arr2)){
-    return arr1.length === arr2? true : false;
+  if (!Array.isArray(arr2)) {
+    return arr1.length === arr2 ? true : false;
   }
   if (arr1.length !== arr2.length) {
     return false;
@@ -23,13 +23,13 @@ const assertArraysEqual = function(arr1,arr2) {
 const middle = function(arr) {
   if (lenCheck(arr,1) || lenCheck(arr,2)) return [];
   let midLength = Math.floor(arr.length / 2);
-  return arr.length % 2 === 0 ? [arr[midLength-1],arr[midLength]] : [arr[midLength]];
+  return arr.length % 2 === 0 ? [arr[midLength - 1],arr[midLength]] : [arr[midLength]];
 };
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5, 6]));
 console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5])); 
+console.log(middle([1, 2, 3, 4, 5]));
 console.log(middle([1]));
 console.log(middle([1, 2]));
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])
+assertArraysEqual(middle([1, 2, 3]),[2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
