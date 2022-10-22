293a38cb6a31b1cea5eeec6a11b80ed93ecc14bc
Francisco <kikopocampo@gmail.com>
2022-10-21 21:11:09 -0600

    can now process nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,21 +3,26 @@ const assertEqual = function(actual, expected) {
     : console.log(`🥲 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const lenCheck = function(arr1,arr2){
+const lenCheck = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } return true;
 };
 
-const eqArrays = function(arr1,arr2){
+const eqArrays = function(arr1,arr2) {
   if (lenCheck(arr1,arr2) === false) return false;
-  for (let i = 0 ; i < arr1.length ; i++){
-    if (arr1[i] !== arr2[i]) return false;
-  };
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i],arr2[i])) return false;
+    }
+    if (!Array.isArray(arr1[i])) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+  }
   return true;
 };
 
 assertEqual('Bootcamp', 'Bootcamp');
-assertEqual(2,1);
 assertEqual(eqArrays([1,4,3],[4,1,3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, [[1, 2, 3, 9], 3]], [1, 2, [[1, 2, 3, 9], 3]]), true);
