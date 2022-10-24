25522e1c739efe53716956934230285bb588c67e
Francisco <kikopocampo@gmail.com>
2022-10-24 14:24:12 -0600

    changed assertion to chai assertion

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,25 @@
 const middle = require('../middle.js');
-const assertArraysEqual = require('../assertArraysEqual.js')
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]),[]);
+  });
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1, 2]),[]);
+  });
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3]),[2]);
+  });
+  it("returns [2,3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2,3]);
+  });
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]),[4]);
+  });
+    
+});
+  
 
 
-//TEST CODE
-
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]),[4]);
