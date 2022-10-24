64cbd0d6dc52008a4abe8f64ca208d36c2d545f6
Francisco <kikopocampo@gmail.com>
2022-10-24 10:44:13 -0600

    refactored function

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,12 @@
-const assertEqual = require('./assertEqual.js')
-
-const lenCheck = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } return true;
-};
-
+//Evaluates two arrays if equal.
 const eqArrays = function(arr1,arr2) {
-  if (lenCheck(arr1,arr2) === false) return false;
+  if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (Array.isArray(arr1[i])) {
       if (!eqArrays(arr1[i],arr2[i])) return false;
     }
-    if (!Array.isArray(arr1[i])) {
-      if (arr1[i] !== arr2[i]) return false;
-    }
+    
+    if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 };
