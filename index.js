5dff01967c7985af868430f727007654ea2a668c
Francisco <kikopocampo@gmail.com>
2022-10-24 16:14:42 -0600

    init commit for index

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 
-
-
 module.exports = {
   head : head,
   tail : tail,
