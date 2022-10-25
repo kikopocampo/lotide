5f9260ad631a62af93968029eadf0f5da7298a2a
Francisco <kikopocampo@gmail.com>
2022-10-24 18:42:20 -0600

    removed test codes

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,4 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`🥳Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🥲Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual')
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
@@ -25,12 +22,5 @@ const firstNames = [
   'Fang',
   'Joe'
 ];
-
-// const result1 = countOnly(firstNames, {'Jason':true,'Karima':true,'Fang':true,'Agouhanna':false});
-// console.log(result1);
-// assertEqual(result1['Jason'], 1);
-// assertEqual(result1['Karima'], undefined);
-// assertEqual(result1['Fang'], 2);
-// assertEqual(result1['Agouhanna'], undefined);
 
 module.exports = countOnly;
