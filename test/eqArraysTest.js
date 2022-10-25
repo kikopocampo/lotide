134dc5e3b120f8f0bd6f5b085088596c7a8a0c19
Francisco <kikopocampo@gmail.com>
2022-10-24 19:24:55 -0600

    init commit for eqArrayTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,23 @@
 const eqArrays = require('../eqArrays.js');
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
-// TEST CODE
-assertEqual('Bootcamp', 'Bootcamp');
-assertEqual(eqArrays([1,4,3],[4,1,3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, [[1, 2, 3, 9], 3]], [1, 2, [[1, 2, 3, 9], 3]]), true);
-assertEqual(eqArrays([1, 'a', 3], [1, 'a', 3]), true);
+describe('#findKeyByValue', () => {
+
+  it('returns true for ([1, 2, 3], [1, 2, 3])', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('returns true for ([1, 2, [[1, 2, 3, 9], 3]], [1, 2, [[1, 2, 3, 9], 3]])', () => {
+    assert.isTrue(eqArrays([1, 2, [[1, 2, 3, 9], 3]], [1, 2, [[1, 2, 3, 9], 3]]));
+  });
+
+  it('returns true for ([1, "a", 3], [1, "a", 3])', () => {
+    assert.isTrue(eqArrays([1, "a", 3], [1, "a", 3]));
+  });
+
+});
+// assertEqual('Bootcamp', 'Bootcamp');
+// assertEqual(eqArrays([1,4,3],[4,1,3]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, [[1, 2, 3, 9], 3]], [1, 2, [[1, 2, 3, 9], 3]]), true);
+// assertEqual(eqArrays([1, 'a', 3], [1, 'a', 3]), true);
