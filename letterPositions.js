4942471f77304f252b01c2ace326511b8c402903
Francisco <kikopocampo@gmail.com>
2022-10-24 18:53:24 -0600

    removed test codes

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,3 @@
-const lenCheck = function(arr1,arr2) {
-  if (!Array.isArray(arr2)) {
-    return arr1.length === arr2 ? true : false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else return true;
-};
-
-const eqArrays = function(arr1,arr2) {
-  if (lenCheck(arr1,arr2) === false) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2)) console.log(`🙆🏻 Assertion Passed: ${arr1} === ${arr2}`);
-  else console.log(`🙅🏻 Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
 const splitSentence = sentence => sentence.split('');
 const letterPositions = function(sentence) {
   const results = {};
@@ -28,15 +6,5 @@ const letterPositions = function(sentence) {
   });
   return results;
 };
-// let test1 = (letterPositions('lighthouse in the house'));
-// console.log(test1);
-// assertArraysEqual(test1.i,[1,11]);
-// assertArraysEqual(test1.n,[12]);
-// assertArraysEqual(test1.e,[9,16,22]);
-// let test2 = (letterPositions('aabb ccdd'));
-// console.log(test2);
-// assertArraysEqual(test2.a,[0,1]);
-// assertArraysEqual(test2.b,[2,3]);
-// assertArraysEqual(test2.d,[7,8]);
 
 module.exports = letterPositions;
